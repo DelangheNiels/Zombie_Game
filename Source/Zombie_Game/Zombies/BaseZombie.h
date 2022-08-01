0c20b55ff6b9ed87d5258f2b40a2bd9436640458
DelangheNiels <delanghe.niels@telenet.be>
2022-08-01 14:36:49 +0200

    Start zombies

---
+++ Source/Zombie_Game/Zombies/BaseZombie.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "../BaseCharacter.h"
+#include "BaseZombie.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZOMBIE_GAME_API ABaseZombie : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
